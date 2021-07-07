6831faf103f6af7933bcd712fcfb5da01ed1c2be
qiujiaaa <ooiqiujia@hotmail.com>
2021-07-07 21:52:02 +0800

    add post schema and middleware multer to handle file uploads

---
+++ server/models/PostModel.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const postSchema = new mongoose.Schema({
+	title: {
+		type: String,
+		require: true,
+		trim: true,
+	},
+	caption: {
+		type: String,
+		trim: true,
+	},
+	img: {
+		data: Buffer,
+		contentType: String,
+		require: true,
+	},
+	createdAt: {
+		type: Date,
+		default: Date.now(),
+	},
+});
+
+module.exports = new mongoose.model('Post', postSchema);
