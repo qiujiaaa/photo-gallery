974db310cd9736d2dbe63fd96883cf92f781fc94
qiujiaaa <ooiqiujia@hotmail.com>
2021-07-07 21:08:53 +0800

    set up basic server and connection to mongodb

---
+++ server/config/db.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+	try {
+		const conn = await mongoose.connect(process.env.MONGO_URI, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+			useFindAndModify: false,
+			useCreateIndex: true,
+		});
+		console.log(`Connected to MongoDB: ${conn.connection.host}`);
+	} catch (err) {}
+};
+
+module.exports = connectDB;
--- server/index.js
+++ server/index.js
@@ -1,1 +1,35 @@
-import express from 'express';
+const express = require('express');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
+
+const connectDB = require('./config/db');
+
+// load config
+dotenv.config({ path: './config/config.env' });
+
+// initialise server
+const app = express();
+
+// morgan for logging
+app.use(morgan('dev'));
+
+// connect to db
+connectDB();
+
+// middleware parse JSON bodies | parse URL-encoded bodies
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Logging
+if (process.env.NODE_ENV === 'development') {
+	app.use(morgan('dev'));
+}
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+	PORT,
+	console.log(
+		`Server running in ${process.env.NODE_ENV} mode on port ${PORT}.`
+	)
+);
