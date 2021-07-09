d662cf3e3bc06ffe45cc2f91fcdf2f3079ef858b
qiujiaaa <ooiqiujia@hotmail.com>
2021-07-09 17:43:13 +0800

    set up grid for dashboard

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,11 +1,53 @@
 import React from 'react';
 import Post from './Post';
+import { Grid, Box, makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+	root: {
+		flexGrow: 1,
+		marginLeft: '80px',
+		marginRight: '80px',
+		marginTop: '50px',
+		marginBottom: '50px',
+	},
+	grid: {
+		alignItems: 'center',
+		justify: 'center',
+	},
+	item: {
+		display: 'flex',
+		justifyContent: 'center',
+	},
+}));
 
 const Dashboard = () => {
+	const classes = useStyles();
+
 	return (
-		<div>
+		<div className={classes.root}>
 			<p>Hello from dashboard</p>
-			<Post />
+			<Grid className={classes.grid} container spacing={3}>
+				<Grid item xs={12} sm={6} md={4}>
+					<Box className={classes.item}>
+						<Post />
+					</Box>
+				</Grid>
+				<Grid item xs={12} sm={6} md={4}>
+					<Box className={classes.item}>
+						<Post />
+					</Box>
+				</Grid>
+				<Grid item xs={12} sm={6} md={4}>
+					<Box className={classes.item}>
+						<Post />
+					</Box>
+				</Grid>
+				<Grid item xs={12} sm={6} md={4}>
+					<Box className={classes.item}>
+						<Post />
+					</Box>
+				</Grid>
+			</Grid>
 		</div>
 	);
 };
