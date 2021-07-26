9a171dd0a8642fc71f8bd41c164ce147d32cc8bb
qiujiaaa <ooiqiujia@hotmail.com>
2021-07-26 23:04:06 +0800

    add basic sign in and logout auth routes on server

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,8 +1,22 @@
 const express = require('express');
-
+const passport = require('passport');
 const router = express.Router();
-const { redirectDashboard } = require('../controllers/authController');
+//const { redirectDashboard } = require('../controllers/authController');
 
-router.get('/', redirectDashboard);
+router.get('/google', passport.authenticate('google', { scope: ['profile'] }));
+
+router.get(
+	'/google/callback',
+	passport.authenticate('google', { failureRedirect: '/' }),
+	(req, res) => {
+		// Successful authentication, redirect to dashboard.
+		res.redirect('http://localhost:3000/dashboard');
+	}
+);
+
+router.get('/logout', (req, res) => {
+	req.logout();
+	res.redirect('/');
+});
 
 module.exports = router;
