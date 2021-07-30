7104815a2b7a2c484a938a923fe4f3a7269ebac4
qiujiaaa <ooiqiujia@hotmail.com>
2021-07-30 22:13:00 +0800

    frontend to do google auth and send token to backend

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,12 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { GoogleLogin } from 'react-google-login';
 import { Button } from '@material-ui/core';
 
+import config from './../../config.json';
+
 const Login = () => {
+	const [auth, setAuth] = useState(false);
+	const [token, setToken] = useState('');
+	const [user, setUser] = useState(null);
+
+	const handleFailure = (error) => {
+		alert(error);
+	};
+
+	const handleSuccess = (response) => {
+		const options = {
+			method: 'POST',
+			body: response.accessToken,
+			mode: 'cors',
+			cache: 'default',
+		};
+		fetch('http://localhost:5000/auth/google', options).then((r) => {
+			const token = r.headers.get('x-auth-token');
+			r.json().then((user) => {
+				if (token) {
+					setAuth(false);
+					setToken(token);
+					setUser(user);
+				}
+			});
+		});
+	};
+
 	return (
 		<div>
 			<Button href="http://localhost:5000/auth/google">
 				Login using Google
 			</Button>
+			<GoogleLogin
+				clientId={config.GOOGLE_CLIENT_ID}
+				buttonText="Login"
+				onSuccess={handleSuccess}
+				onFailure={handleFailure}
+			/>
 		</div>
 	);
 };
